e937f89dbe0640f3bbb28cbf1f1b434c1be17fea
Charles Rice <charlesrice@Charless-iMac.local>
2014-09-16 15:53:11 +0100

    Initial Commit

---
+++ ToDoList/XYZAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  XYZAppDelegate.h
+//  ToDoList
+//
+//  Created by Charles Rice on 16/09/2014.
+//  Copyright (c) 2014 Cake Solutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XYZAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ToDoList/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ToDoList
+//
+//  Created by Charles Rice on 16/09/2014.
+//  Copyright (c) 2014 Cake Solutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "XYZAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([XYZAppDelegate class]));
+    }
+}
